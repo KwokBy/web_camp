a95a2802678442ae6b970b33a87a7d7ca258ecac
Kwok <1042936527@qq.com>
2019-04-06 00:53:19 +0800

    Aqueue

--- 第三周/AQueue.c
+++ 第三周/AQueue.c
@@ -4,7 +4,7 @@
 
 void InitAQueue(AQueue *Q)
 {
-   Q = (AQueue*)malloc(MAXQUEUE*sizeof(AQueue));
+   //Q = (AQueue*)malloc(MAXQUEUE*sizeof(AQueue));
    Q->front=0;
    Q->rear=0;
    Q->length=0;
@@ -12,26 +12,45 @@ void InitAQueue(AQueue *Q)
 
 void DestoryAQueue(AQueue *Q)
 {
-    free(Q);
-    printf("销毁成功\n");
+    if(Q->length<0||Q->length>10)
+    {
+    	printf("请先初始化在销毁\n");
+	}
+	else
+	{
+       Q->length=-1;
+       printf("销毁成功\n");
+    }
 }
 
 Status IsFullAQueue(const AQueue *Q)
 {
-	return (Q->rear+1)%MAXQUEUE == Q->front;
+   if((Q->rear+1)%MAXQUEUE == Q->front)
+   {
+   	   printf("队列满\n");
+	   return TRUE;
+   }
+   return FALSE;
+	
 } 
 
 Status IsEmptyAQueue(const AQueue *Q)
 {
-	return Q->front ==Q->rear;
+     if( Q->front==Q->rear)
+     {
+     	printf("队列为空\n");
+		 return TRUE;
+	 }
+	 return FALSE;
 }
 Status GetHeadAQueue(AQueue *Q, void *e)
 {
 	if(IsEmptyAQueue(Q))
 	{
+		printf("队列为空\n");
 		return FALSE;
 	}
-	e = Q->data;
+	memcpy(e,Q->data+Q->front,sizeof(*e));
 	return TRUE;
 }
 
@@ -47,11 +66,15 @@ Status EnAQueue(AQueue *Q, void *data)
    	    printf("队列满\n");
 		return FALSE;
    }
+   if(Q->length<0||Q->length>10)
+   {
+   	printf("目前队列未存在，请先初始化\n");
+   	return FALSE;
+   }
+   memcpy(Q->data+Q->rear,data,sizeof(*data));
    Q->rear = (Q->rear+1)%MAXQUEUE;
-   Q->data[Q->rear] = data;
    Q->length++;
    printf("入队成功\n");
-   printf("d======%d\n",*(int*)data);
    return TRUE;	
 }
 Status DeAQueue(AQueue *Q)
@@ -61,14 +84,22 @@ Status DeAQueue(AQueue *Q)
 		printf("队列为空\n");
 		return FALSE;
 	}
+	if(Q->length<0||Q->length>10)
+   {
+   	printf("目前队列未存在，请先初始化\n");
+   }
 	
 	Q->front = (Q->front+1)%MAXQUEUE;
+	Q->length--;
+	//printf("fro======%d\n",Q->front);
 	printf("出队成功\n");
 	return TRUE;
 }
 void ClearAQueue(AQueue *Q)
 {
+   
    Q->rear = Q->front;
+   Q->length=0;
    printf("清空成功\n"); 
 }
 Status TraverseAQueue(const AQueue *Q, void (*APrint)(void *q))
@@ -80,33 +111,71 @@ Status TraverseAQueue(const AQueue *Q, void (*APrint)(void *q))
 	if(Q==NULL)
 	{
 		return FALSE;
-	}
+	} 
 	int num = Q->front;
 	while(num!=Q->rear)
 	{
-		APrint(Q->data[num]);
+		//printf("a====%d\n",*(int *)(Q->data[num]));
+		//APrint(Q->data[num]); 
+		APrint(Q->data+num);
 	     num = (num+1)%MAXQUEUE;
 	}
 	return TRUE;
 }
 void APrint(void *q)
 {
-	printf("请选择你要的数据类型\n");
-	show(); 
-	int sel;
-	scanf("%d",&sel);
-	if(sel==1)
-	{
-		printf("%d\n",*(int*)q);
-	}
-	if(sel==2)
-	{
-	   printf("%c\n",*(char*)q);	
-	}
+     int val1=1;
+	 if(val1==1)
+     {
+     	printf("%d  ",*(int*)q);
+     }
+     else if(val1==2)
+     {
+     	printf("%c  ",*(char*)q);
+	 }
 }
 
-void show()
-{
-	printf("1、int\n");
-	printf("2、char\n")	;		
-}                    
+void show(){
+	printf(" ***************************************************************\n");
+	printf(" *                                                             *\n");
+	printf(" ***********************泛型顺序队列****************************\n");
+	printf(" *                                                             *\n");
+	printf(" *************************选择菜单******************************\n");
+	printf(" *                                                             *\n");
+	printf(" *                         1、初始化队列                       *\n");
+	printf(" *                         2、销毁队列                         *\n");
+	printf(" *                         3、判断队列是否满                   *\n");
+	printf("                           4、判断队列是否空                   *\n");
+	printf(" *                         5、查看队头元素                     *\n");
+	printf(" *                         6、 队列长度                        *\n");
+	printf(" *                         7、 入队                            *\n");
+	printf(" *                         8、 出队                            *\n");
+	printf(" *                         9、 清空队列                        *\n");
+	printf(" *                         10、 遍历队列                       *\n");
+	printf(" *                                                             *\n");
+	printf(" ***************************************************************\n");
+	printf(" *                     请输入你想要进行的功能                  *\n");
+	printf(" ***************************************************************\n");
+}
+
+int  inputerror(){
+char a[20];
+    scanf("%s",a);
+    int val = 0;
+    int i; 
+    for( i=0;a[i];i++)
+    {
+        if((a[i]<'0'||a[i]>'9'))
+        {
+            memset(a,0,80);
+            printf("??????\n");
+            scanf("%s",a);
+        }
+    }
+    for(i=0;a[i];i++){
+        val = val*10+a[i]-'0';
+    }
+    return val;
+}
+
+
--- 第三周/main.c
+++ 第三周/main.c
@@ -2,18 +2,125 @@
 #include <stdlib.h>
 #include"C:\Users\kwok\Desktop\QG训练营第三次培训\QG训练营第三次培训\代码和头文件\头文件\AQueue.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
-	
 	AQueue Q;
-	InitAQueue(&Q);
-	int data= 6;
-	int e;
-	EnAQueue(&Q, &data);
-	IsFullAQueue(&Q);
-	GetHeadAQueue(&Q, &e);
-	void * pfun;
-	pfun = &APrint;
-	TraverseAQueue(&Q, pfun);
-	return 0;
+	 int val1;
+	int seltion,n=1;
+	void *pfun = *APrint;
+	printf("请输入你想要的类型(1、int\ 2、char)\n");
+    scanf("%d",&val1);
+	while(n==1){ 
+     	system("cls");
+	    show();
+	  seltion = inputerror();
+	  while(seltion>10||seltion<0){
+	  	  printf("非法输入，请输入1~10的数字\n");
+		  seltion = inputerror();
+	  }
+	  switch(seltion){
+		case 1:
+			{
+				InitAQueue(&Q); 
+				printf("初始化成功\n");
+				break;
+			}
+		case 2:
+			{
+			   DestoryAQueue(&Q);
+			   break;
+			}
+		case 3:
+			{
+			   IsFullAQueue(&Q);
+			   break;	
+			}
+		case 4:
+			{
+				IsEmptyAQueue(&Q);
+				break;
+			}
+		case 5:
+			{
+				
+				//printf("sizeof======%d\n",sizeof(Q.data));
+				if(val1==1)
+				{
+				   int e;
+				   GetHeadAQueue(&Q, &e);
+				   printf("队头元素为int型的:%d\n",e);	
+				}
+				else if(val1==2)
+				{
+				   char e;
+				   GetHeadAQueue(&Q, &e);
+				   printf("队头元素为int型的:%c\n",e);	
+				}
+				else if(val1==3)
+				{
+					double e;
+					GetHeadAQueue(&Q, &e);
+				    printf("队头元素为int型的:%f\n",e);
+				}
+				break;
+			}
+		case 6:
+			{
+				printf("队列长度为：%d\n",LengthAQueue(&Q));
+				break; 
+			}
+		case 7:
+		    {
+		    	printf("请输入想要入队的个数（最多10个）\n");
+		    	int size = inputerror();
+		    	int i;
+		    	
+		    	for(i=0;i<size;i++)
+		    	{
+		    		
+		    		if(val1==1)
+		    		{
+		    			int data;
+		    			printf("请输入int型第%d数据\n",i+1);
+		    			scanf("%d",&data);
+						EnAQueue(&Q, &data);
+					}
+					else if(val1==2)
+					{
+					   char  data;
+					   scanf("%c",&data);
+					   printf("请输入char型第%d数据\n",i+1);
+					   scanf("%c",&data);
+					   EnAQueue(&Q, &data);	
+					}
+					else if(val1==3)
+					{
+					   char  data;
+					   scanf("%f",&data);
+					   printf("请输入char型第%d数据\n",i+1);
+					   scanf("%f",&data);
+					   EnAQueue(&Q, &data);	
+					}
+					
+				}
+				
+		    	break;
+			}		
+		case 8:
+			{
+			    DeAQueue(&Q);
+				break;	
+			}
+		case 9:
+			{
+				ClearAQueue(&Q);
+				break;
+			}
+		case 10:
+			{
+				TraverseAQueue(&Q, pfun);
+			}
+		} 	
+		printf("你想要继续吗(1:yes/other num:no)");
+		n = inputerror();
+	}
 }
