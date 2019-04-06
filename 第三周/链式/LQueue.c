84b5e90ee0eff4fc37acf60f1913783df6baed14
Kwok <1042936527@qq.com>
2019-04-06 11:54:13 +0800

    LQueue

---
+++ 第三周/链式/LQueue.c
@@ -0,0 +1,157 @@
+#include"C:\Users\kwok\Desktop\QG训练营第三次培训\QG训练营第三次培训\代码和头文件\头文件\LQueue.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void InitLQueue(LQueue *Q)
+{
+      Q->front	= (Node*)malloc(sizeof(Node));
+      if(!Q)
+      {
+      	return FALSE;
+	  }
+	  Q->rear = Q->front;
+	  Q->front->next = NULL;
+	  Q->length = 0; 
+}
+void DestoryLQueue(LQueue *Q)
+{
+    ClearLQueue(Q);
+    free(Q->front);
+    Q->front = NULL;
+    Q->rear = NULL;
+    Q->length = 0;
+    Q=NULL;
+	LQueue *p;
+	
+}
+Status IsEmptyLQueue(const LQueue *Q)
+{
+	if(Q->length==0)
+	{
+		printf("队列为空\n");
+		return TRUE;
+	}
+	return FALSE;
+}
+Status GetHeadLQueue(LQueue *Q, void *e)
+{
+   if(Q->length==0)
+   {
+   	return FALSE;
+   }
+   e = Q->front->data;
+   return TRUE;
+}
+int LengthLQueue(LQueue *Q)
+{
+   return	Q->length;
+}
+Status EnLQueue(LQueue *Q, void *data)
+{
+   LQueue * p = (Node*)malloc(sizeof(Node));
+   Q->rear->data = data;
+   Q->rear->next = p;
+   Q->rear = p;
+   Q->length++;
+   //printf("in=======%d\n",*(int *)(Q->rear->data));
+   return TRUE;	
+}
+Status DeLQueue(LQueue *Q)
+{
+   if(IsEmptyLQueue(Q))
+   {
+   	   printf("队列为空，无法出队\n");
+	   return FALSE;
+   }
+  else
+	  {
+	  	Node *freeNode = Q->front;
+        Q->front = Q->front->next;
+        free(freeNode);
+        Q->length--;
+	    return TRUE;
+	  }  
+}
+void ClearLQueue(LQueue *Q)
+{
+	while(Q->front==Q->rear)
+	{
+		Node * p = Q->front;
+		Q->front =Q->front->next;
+		free(p); 
+	}
+	Q->length=0;
+}
+Status TraverseLQueue(const LQueue *Q, void (*LPrint)(void *q))
+{
+	 if(IsEmptyLQueue(Q))
+   {
+   	   printf("队列为空\n");
+	   return FALSE;
+   }
+   LQueue *p = Q;
+   while(p->front!=p->rear)
+   {
+   	LPrint(p->front->data);
+   	p->front= p->front->next;
+   }
+}
+  
+void LPrint(void *q)
+{
+	extern val1;
+	if(val1==1)
+	{
+		printf("%d  ",*(int *)q);
+	}
+	if(val1==2)
+	{
+		printf("%c  ",*(char*)q);
+	}
+}
+
+
+void show(){
+	printf(" ***************************************************************\n");
+	printf(" *                                                             *\n");
+	printf(" ***********************泛型链式队列****************************\n");
+	printf(" *                                                             *\n");
+	printf(" *************************选择菜单******************************\n");
+	printf(" *                                                             *\n");
+	printf(" *                         1、初始化队列                       *\n");
+	printf(" *                         2、销毁队列                         *\n");
+	printf(" *                         3、判断队列是否满                   *\n");
+	printf("                           4、判断队列是否空                   *\n");
+	printf(" *                         5、查看队头元素                     *\n");
+	printf(" *                         6、 队列长度                        *\n");
+	printf(" *                         7、 入队                            *\n");
+	printf(" *                         8、 出队                            *\n");
+	printf(" *                         9、 清空队列                        *\n");
+	printf(" *                         10、 遍历队列                       *\n");
+	printf(" *                                                             *\n");
+	printf(" ***************************************************************\n");
+	printf(" *                     请输入你想要进行的功能                  *\n");
+	printf(" ***************************************************************\n");
+}
+
+int  inputerror(){
+char a[20];
+    scanf("%s",a);
+    int val = 0;
+    int i; 
+    for( i=0;a[i];i++)
+    {
+        if((a[i]<'0'||a[i]>'9'))
+        {
+            memset(a,0,80);
+            printf("非法输入，请重输\n");
+            scanf("%s",a);
+        }
+    }
+    for(i=0;a[i];i++){
+        val = val*10+a[i]-'0';
+    }
+    return val;
+}
+
---
+++ 第三周/链式/main.c
@@ -0,0 +1,129 @@
+#include"C:\Users\kwok\Desktop\QG训练营第三次培训\QG训练营第三次培训\代码和头文件\头文件\LQueue.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int val1;
+int main(int argc, char *argv[]) {
+	LQueue Q;
+	 
+	int seltion,n=1;
+	void *pfun = *LPrint;
+	printf("请输入你想输入的数据类型(1.int\ 2.char)\n");
+    scanf("%d",&val1);
+	while(n==1){ 
+     	system("cls");
+	    show();
+	  seltion = inputerror();
+	  while(seltion>10||seltion<0){
+	  	  printf("请输入1~10的有效数字\n");
+		  seltion = inputerror();
+	  }
+	  switch(seltion){
+		case 1:
+			{
+				InitLQueue(&Q); 
+				printf("初始化成功\n");
+				break;
+			}
+		case 2:
+			{
+			   DestoryLQueue(&Q);
+			   break;
+			}
+		/*case 3:
+			{
+			   IsFullLQueue(&Q);
+			   break;	
+			}*/ 
+		case 4:
+			{
+				IsEmptyLQueue(&Q);
+				break;
+			}
+		case 5:
+			{
+				
+				//printf("sizeof======%d\n",sizeof(Q.data));
+				if(val1==1)
+				{
+				   int e;
+				   GetHeadLQueue(&Q, &e);
+				   printf("队头int型元素:%d\n",e);	
+				}
+				else if(val1==2)
+				{
+				   char e;
+				   GetHeadLQueue(&Q, &e);
+				   printf("队头char型元素:%c\n",e);	
+				}
+				else if(val1==3)
+				{
+					double e;
+					GetHeadLQueue(&Q, &e);
+				    printf("队头double型元素:%f\n",e);
+				}
+				break;
+			}
+		case 6:
+			{
+				printf("队列长度为%d\n",LengthLQueue(&Q));
+				break; 
+			}
+		case 7:
+		    {
+		    	printf("请输入你想要的队列长度\n");
+		    	int size = inputerror();
+		    	int i;
+		    	for(i=0;i<size;i++)
+		    	{
+		    		
+		    		if(val1==1)
+		    		{
+		    			int data;
+		    			printf("请输入int型%d个数据\n",i+1);
+		    			scanf("%d",&data);
+						EnLQueue(&Q, &data);
+					//	printf("data=========%d ",data);
+					}
+					else if(val1==2)
+					{
+					   char  data;
+					   scanf("%c",&data);
+					   printf("请输入char型第%d个数据\n",i+1);
+					   scanf("%c",&data);
+					   EnLQueue(&Q, &data);	
+					}
+					else if(val1==3)
+					{
+					   char  data;
+					   scanf("%f",&data);
+					   printf("请输入double型第%d个数据\n",i+1);
+					   scanf("%f",&data);
+					   EnLQueue(&Q, &data);	
+					}
+					
+				}
+				
+		    	break;
+			}		
+		case 8:
+			{
+			    DeLQueue(&Q);
+				break;	
+			}
+		case 9:
+			{
+				ClearLQueue(&Q);
+				break;
+			}
+		case 10:
+			{
+				TraverseLQueue(&Q, pfun);
+			}
+		} 	
+		printf("你还想继续吗(1:yes/other num:no)");
+		n = inputerror();
+	}
+}
+
